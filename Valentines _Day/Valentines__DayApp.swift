bc9ccb946ea6409db39467f93bbd0ef12ad479c2
sallylpez <sallylpez@gmail.com>
2025-02-12 18:43:24 -0600

    Initial Commit

---
+++ Valentines _Day/Valentines__DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valentines__DayApp.swift
+//  Valentines _Day
+//
+//  Created by Karla Gallegos on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Valentines__DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Valentines _DayTests/Valentines__DayTests.swift
@@ -0,0 +1,17 @@
+//
+//  Valentines__DayTests.swift
+//  Valentines _DayTests
+//
+//  Created by Karla Gallegos on 12/02/25.
+//
+
+import Testing
+@testable import Valentines__Day
+
+struct Valentines__DayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

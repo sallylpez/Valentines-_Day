092301ce35412504a71db9e6ac9df6748cfebe4e
sallylpez <sallylpez@gmail.com>
2025-02-13 11:28:41 -0600

    Agregamos assets

--- Valentines _Day/ContentView.swift
+++ Valentines _Day/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Valentines _Day
+//  Valentines_Day
 //
 //  Created by Karla Gallegos on 12/02/25.
 //
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var buttonSize: CGFloat = 15
+    @State private var isAlertShowing: Bool = false
+    @State private var viewID = UUID() // Identificador para reiniciar la vista
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // Fondo de color
+            Color.pink.opacity(0.3)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("¿Quieres ser mi San Valentín?")
+                    .font(.title)
+                    .bold()
+                    .padding(.bottom, 10)
+                
+                Image("mouse")
+                    .resizable()
+                    .frame(width: 300, height: 300)
+                
+                HStack {
+                    Button(action: {
+                        isAlertShowing = true
+                    }, label: {
+                        Text("Sí")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: buttonSize*2))
+                            .padding()
+                            .frame(minWidth: 80)
+                            .background(Color.green)
+                            .clipShape(Capsule())
+                    })
+                    
+                    Button(action: {
+                        updateButtonSize()
+                    }, label: {
+                        Text("No")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .font(.system(size: 15))
+                            .padding()
+                            .frame(minWidth: 80)
+                            .background(Color.red)
+                            .clipShape(Capsule())
+                    })
+                }
+                .padding()
+            }
         }
-        .padding()
+        .id(viewID) // Agregar ID para forzar la reconstrucción
+        .alert("Gano el amor", isPresented: $isAlertShowing) {
+            Button("OK") { restartApp() }
+        } message: {
+            Text("Te amo ❤️")
+        }
+    }
+    
+    func updateButtonSize() {
+        buttonSize += 5
+    }
+    
+    func restartApp() {
+        viewID = UUID() // Cambiar el ID fuerza la reconstrucción de la vista
+        buttonSize = 15 // Reiniciar el tamaño del botón
     }
 }
 
